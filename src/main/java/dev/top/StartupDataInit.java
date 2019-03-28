2ced5ad9fa056cc2e195030ac314d0421b400821
Mélanie D <44894678+Morue@users.noreply.github.com>
2019-03-28 14:16:37 +0100

    maj collegue

--- src/main/java/dev/top/StartupDataInit.java
+++ src/main/java/dev/top/StartupDataInit.java
@@ -6,28 +6,28 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import dev.top.entities.Collegue;
-import dev.top.entities.Version;
 import dev.top.repos.CollegueRepo;
-import dev.top.repos.VersionRepo;
+
 
 @Component
 public class StartupDataInit {
-
-    @Autowired
-    VersionRepo versionRepo;
     @Autowired
     CollegueRepo collegueRepo;
 
     @EventListener(ContextRefreshedEvent.class)
     public void init() {
 
-        this.versionRepo.save(new Version("v1"));
-        this.versionRepo.save(new Version("v2"));
-        this.versionRepo.save(new Version("v3"));
-        this.versionRepo.save(new Version("v4"));
+
         
         this.collegueRepo.save(new Collegue(1, 5000, "https", "pseudo1"));
         this.collegueRepo.save(new Collegue(2, 1000, "https", "pseudo2"));
+        
+        this.collegueRepo.save(new Collegue(3, 5000, "https://media.giphy.com/media/8TRhWZqy5vmU0/giphy.gif", "Mabel"));
+        this.collegueRepo.save(new Collegue(4, 4500, "https://media.giphy.com/media/12aZlLvMQtb9e0/giphy.gif", "Dipper"));
+        this.collegueRepo.save(new Collegue(5, 3000, "https://media.giphy.com/media/g1PbvXNZnzQUE/giphy.gif", "Stanley"));
+        this.collegueRepo.save(new Collegue(6, 20000, "https://media.giphy.com/media/mC2SBfNzJkdC8/giphy.gif", "Bill"));
+        this.collegueRepo.save(new Collegue(7, 1000, "https://media.giphy.com/media/DbvmvdRnaPUOs/giphy.gif", "Gideon"));
+        this.collegueRepo.save(new Collegue(8, 8000, "https://media.giphy.com/media/YC15BV6WJRqQo/giphy.gif", "Stanford"));
 
     }
 }
