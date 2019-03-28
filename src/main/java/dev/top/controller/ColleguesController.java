6034b62ffe1ac14402c6e195551e8910346c71a9
Mélanie D <44894678+Morue@users.noreply.github.com>
2019-03-28 11:58:56 +0100

    fix bug

--- src/main/java/dev/top/controller/ColleguesController.java
+++ src/main/java/dev/top/controller/ColleguesController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import dev.top.service.CollegueService;
 
 
 @RestController
 @RequestMapping("/collegues")
-public class CollegueController {
+public class ColleguesController {
 
 	@Autowired
     private CollegueService collegueService;
