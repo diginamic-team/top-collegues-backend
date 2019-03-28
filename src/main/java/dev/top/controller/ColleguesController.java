25e49c26bfa39a601ebe258d6eeccb4f952a62b7
Mélanie D <44894678+Morue@users.noreply.github.com>
2019-03-28 15:04:03 +0100

    ajout CrossOrigin

--- src/main/java/dev/top/controller/ColleguesController.java
+++ src/main/java/dev/top/controller/ColleguesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import dev.top.entities.Collegue;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.top.service.CollegueService;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("/collegues")
 public class ColleguesController {
